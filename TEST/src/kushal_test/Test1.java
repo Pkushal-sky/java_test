7cf9f20351eca3c91ffcfd34f9fc643f20d5ef70
Pkushal-sky <kushalbpatel786@gmail.com>
2019-12-12 18:07:35 -0600

    first commit

---
+++ TEST/src/kushal_test/Test1.java
@@ -0,0 +1,13 @@
+package kushal_test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("1" +  1);
+		System.out.println('1' +  1);
+		System.out.println("1" +  1 + 1);
+		System.out.println("1" +  (1 + 1));
+		System.out.println('1' +  1 + 1);
+	}
+
+}
