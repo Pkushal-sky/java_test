092b680f7754454952e978d576c1a9441f18f995
Pkushal-sky <kushalbpatel786@gmail.com>
2019-12-12 18:11:35 -0600

    add code from test1

--- TEST/src/kushal_test/Test2.java
+++ TEST/src/kushal_test/Test2.java
@@ -3,8 +3,8 @@ package kushal_test;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("1" +  1 + 1);
+		System.out.println("1" +  (1 + 1));
 	}
 
 }
