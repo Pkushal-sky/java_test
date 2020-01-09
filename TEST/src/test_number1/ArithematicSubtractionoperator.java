20580ae013582338030b2d32d0e2f310b58f45d3
Pkushal-sky <kushalbpatel786@gmail.com>
2020-01-09 15:22:01 -0600

    arithematicOperate

---
+++ TEST/src/test_number1/ArithematicSubtractionoperator.java
@@ -0,0 +1,17 @@
+package test_number1;
+//import java.utill.Random;
+public class ArithematicSubtractionoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = Math.random();
+	    double b = Math.random();	
+	    System.out.println("a ="+ a);
+	    System.out.println("b ="+ b);
+		System.out.println("a + b = " + (a + b));
+		
+
+	}
+
+}
---
+++ TEST/src/test_number1/CrateRandomNumber.java
@@ -0,0 +1,26 @@
+package test_number1;
+
+public class CrateRandomNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		 
+			for (int  i=0; i<1; i++ )
+		
+				
+			{
+				int randomInt = (int)(10.0 * Math.random());
+			      System.out.println(" random number : " + randomInt );
+			      
+			}
+
+		}
+			
+	}
+		
+
+	
+
+
+
+
